58f66427712461fc80f6d98d2969e45b927a5f68
carmen-tm <carmentorremolina@gmail.com>
2019-06-02 18:14:05 +0200

    Reorganize components in folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Prueba from './Prueba';
+import Prueba from './components/PruebaPie';
 import PruebaBars from './components/PruebaBars';
 
 ReactDOM.render(
